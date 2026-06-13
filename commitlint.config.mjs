7aa472c4611546b3d61c0608997cac8e889fdb64
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-06-13 10:34:46 +0200

    fix: ci

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,9 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [1, 'always', 120],
+    'body-max-line-length': [0, 'always'],
+    'footer-max-line-length': [0, 'always'],
+    'subject-case': [0, 'always'],
+  },
 };
